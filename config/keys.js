66e65afb83c67feaa4dafcdb88dde3dcd7e0cae2
Thomas Lawrence <thomas66690801@gmail.com>
2018-12-15 12:46:41 +1100

    first commit incl backend

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+//watch for spacing after curly bracket)
+// mongoose.connect("mongodb://thomas:thomas1@ds113630.mlab.com:13630/mern-stack-shopping-list", {useNewUrlParser: true});
+
+module.exports ={mongoURI: 'mongodb://thomas:thomas1@ds113630.mlab.com:13630/mern-stack-shopping-list'};
+
+
+// mongoose.connect("mongodb://localhost:27017/YourDB", { useNewUrlParser: true });

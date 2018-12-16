2d6dceb0ce070a08f960e1e5e42cdeee852f1861
Thomas Lawrence <thomas66690801@gmail.com>
2018-12-16 22:59:50 +1100

    adding redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 import AppNavBar from './components/AppNavBar';
 import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/ItemModal';
+import {Container} from 'reactstrap';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavBar />
-        <ShoppingList />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavBar/>
+            <Container>
+              <ItemModal/>
+              <ShoppingList/>
+            </Container>
+        </div>
+      </Provider>
     );
-  }
-}
+  };
+};
 
 export default App;

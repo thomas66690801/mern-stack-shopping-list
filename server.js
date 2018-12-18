3f7ec2d1d00e8ddb3afca9a1deb2af418a54aed6
Thomas Lawrence <thomas66690801@gmail.com>
2018-12-19 10:35:46 +1100

    adding he

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 //DB config -- pointint to the keys in config folder
 const db = require('./config/keys').mongoURI;
-
+const path = require('path');
 
 //connect to Mongo
 mongoose
@@ -22,6 +22,17 @@ mongoose
 
 //Use Routes
 app.use('/api/items', items);
+
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res)=> {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+
+}
 
 
   const port = process.env.PORT || 5000;

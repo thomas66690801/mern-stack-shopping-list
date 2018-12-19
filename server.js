f490007a293e1e5969fc991d3418e83e4ac76b5f
Thomas Lawrence <thomas66690801@gmail.com>
2018-12-19 13:20:48 +1100

    changing static details

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use('/api/items', items);
 if(process.env.NODE_ENV === 'production') {
   //Set static folder
   // app.use(express.static('client/build'));
-  app.use('/static', express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
 
   app.get('*', (req, res)=> {

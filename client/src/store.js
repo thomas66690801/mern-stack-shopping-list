3441524b6bbc535b32e79a6c0af894fb58dde926
Thomas Lawrence <thomas66690801@gmail.com>
2018-12-19 21:42:58 +1100

    adding production code for redux

--- client/src/store.js
+++ client/src/store.js
@@ -5,19 +5,12 @@ import rootReducer from './reducers';
 const initialState ={};
 const middleware =[thunk];
 
-// this is for development
-// const store = createStore(rootReducer, initialState, compose(
-//   applyMiddleware(...middleware),
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// ));
-
-
-
-// this is for production
-import { devToolsEnhancer } from 'redux-devtools-extension/logOnlyInProduction';
-const store = createStore(reducer, /* preloadedState, */ devToolsEnhancer(
-  // options like actionSanitizer, stateSanitizer
+const store = createStore(rootReducer, initialState, compose(
+  applyMiddleware(...middleware)
+ // ,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
+
+
 
 
 
